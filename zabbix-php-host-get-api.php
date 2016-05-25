317c51e79dafd33bb3a06780da635d224871414a
miguelacaro <miguelacaro@gmail.com>
2016-05-25 03:13:54 +0200

    Update zabbix-php-host-get-api.php

--- zabbix-php-host-get-api.php
+++ zabbix-php-host-get-api.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * 本接口可以通过zabbix api获取zabbix host列表
- * author: ivon lee
- * mail: root@54im.com
- * url: http://54im.com
+ *  Zabbix Php Api
+ * author: Alejandro Caro
+ * 
  */
-$url = 'http://zabbix.54im.com/zabbix/api_jsonrpc.php';
+ 
+ // API Settings 
+$url = 'http://zabbixserver/zabbix/api_jsonrpc.php';
 $header = array("Content-type: application/json-rpc");
 $user = 'Admin';
 $password = 'zabbix';
 
-//把curl定义为一个函数，方便后面的接口调用
+//curl Function
 function Curl($url,$header,$info){
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL, $url);
@@ -24,12 +25,12 @@ function Curl($url,$header,$info){
 }
 
 /**
- * 你可以在linux上通过以下方式测试zabbix接口，这个接口是用户登录接口，会得到一串id，后续接口要用到
- * curl -i -X POST -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0","method":"user.login","params":{"user":"Admin","password":"zabbix"},"auth": null,"id":0}' http://zabbix.54im.com/zabbix/api_jsonrpc.php
+ * Same Curl command from command line
+ * curl -i -X POST -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0","method":"user.login","params":{"user":"Admin","password":"zabbix"},"auth": null,"id":0}' http://zabbixserver/zabbix/api_jsonrpc.php
  **/
 
 /**
-* 这里是要post到zabbix接口的数据，php中可以用数组来定义，也可以直接用json格式的字符串
+* JSON array format
 $logininfo = array(
   'jsonrpc' => '2.0',
   'method' => 'user.login',
@@ -44,16 +45,21 @@ $data = json_encode($logininfo);
 
 $logininfo = '{"jsonrpc": "2.0","method":"user.login","params":{"user":"Admin","password":"zabbix"},"auth": null,"id":0}';
 $result = Curl($url,$header,$logininfo);
-//返回的$result是一个对象，我们需要把对象中的result拿出来，下面获取服务器列表要用到
+//Result is an object
 $token = $result->result;
 
 
 /**
-*下面这个接口是获取zabbix host列表
-*可以在linux上试试 curl -i -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"host.get","params":{"output":["hostid","name"],"filter":{"host":""}},"auth":"e81981f57500ede530007104df178f08","id":1}' http://zabbix.54im.com/zabbix/api_jsonrpc.php
-*上面接口我们用的json字符串格式，下面我们尝试下数组方式，注意要将数组转成json格式
-*$hostinfo = '{"jsonrpc":"2.0","method":"host.get","params":{"output":["hostid","name"],"filter":{"host":""}},"auth":"e81981f57500ede530007104df178f08","id":1}';
-**/
+* HOST LIST
+* Curl from command line
+* curl -i -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"host.get","params":{"output":["hostid","name"],"filter":{"host":""}},"auth":"e81981f57500ede530007104df178f08","id":1}' http://zabbix.54im.com/zabbix/api_jsonrpc.php
+*
+*  JSON STRING FORMAT 
+* $hostinfo = '{"jsonrpc":"2.0","method":"host.get","params":{"output":["hostid","name"],"filter":{"host":""}},"auth":"e81981f57500ede530007104df178f08","id":1}';
+*
+* JSON ARRAY FORMAT
+*/
+
 $hostinfo = array(
     'jsonrpc' => '2.0',
     'method' => 'host.get',
